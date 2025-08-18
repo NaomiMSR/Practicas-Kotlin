91f436d6398b7004b3620b69bed548dbbccb44dc
NaomiMSR <santosnaomi0311@gmail.com>
2025-08-18 16:29:07 -0600

    Create reto1.kt
    Ejercicio de clase

---
+++ reto1.kt
@@ -0,0 +1,13 @@
+package practicas
+import  kotlin.math.*
+
+fun main(){
+    var martin = 50
+    var pedro = martin/2
+    var resultado  = martin + pedro
+    var juan = resultado * 2
+
+    println("Juan tiene $juan dinero")
+    println("pedro tiene $pedro dinero")
+    println("martin tiene $martin dinero")
+}
